23d5ab9583b0ce8d2d807900f057e7ed5e7937bf
Lt-Leinad <107621905+Lt-Leinad@users.noreply.github.com>
2023-01-11 13:24:26 +0000

    13:24

---
+++ services/script.js
@@ -0,0 +1,10 @@
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const xBtn = document.querySelector(".x-out-btn");
+
+xBtn.addEventListener("click", function () {
+  console.log("click");
+});
+
+hamburgerIcon.addEventListener("click", function () {
+  console.log("click");
+});
