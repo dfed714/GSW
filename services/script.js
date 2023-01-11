e212d95296fc5c846a2a62fd3c513e399c1d2ecb
Lt-Leinad <107621905+Lt-Leinad@users.noreply.github.com>
2023-01-11 18:22:28 +0000

    18:22

--- services/script.js
+++ services/script.js
@@ -3,13 +3,13 @@ const hamburgerIcon = document.querySelector(".hamburger-icon");
 const xBtn = document.querySelector(".x-out-btn");
 
 hamburgerIcon.addEventListener("click", function () {
-  console.log("bars clicked");
   menuTab.classList.remove("display-none");
   xBtn.classList.remove("display-none");
 });
 
 xBtn.addEventListener("click", function () {
-  console.log("x clicked");
   menuTab.classList.add("display-none");
   xBtn.classList.add("display-none");
 });
+
+console.log(document.getElementsByClassName("nav-bar"));
